74092ac5e14244689f63d930f31634729a88c857
Jeremy Black <Jeremy@highridgewebdesign.com>
2026-05-07 16:01:47 -0400

    Fix 1: Add phone number and update CTA in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
-import { Menu, X } from "lucide-react";
+import { Menu, X, Phone } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import BrandLogo from "./BrandLogo";
